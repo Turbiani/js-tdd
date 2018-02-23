56f790cee2bd2bfbe098c61382d575c7a7313050
Turbiani <leo.turbiani@gmail.com>
2018-02-22 23:35:02 -0300

    hooks na pratica

--- trabalhando-com-tdd/tests/main.spec.js
+++ trabalhando-com-tdd/tests/main.spec.js
@@ -1,22 +1,65 @@
-/* global it describe context */
+/* global it describe before after beforeEach afterEach */
 describe('Main', () => {
-  describe('Method A', () => {
-    context('Context 1', () => {
-      it('should happen anything', () => {
-        throw new Error('just on error');
-      });
-    });
+  let arr;
 
-    context.only('Context 2', () => {
-      it.skip('should happen blablabla', () => {
-        throw new Error('just on error');
-      });
-
-      it('should happen mimimi', () => {
-      });
-    });
+  // roda uma vez, antes do bloco
+  before(() => {
+    // inicia uma conexão no banco
+    // criar um conjunto de dados
   });
 
-  describe('Method B  ', () => {
+  // roda uma vez, depois do bloco
+  after(() => {
+    // fecha conexão do banco
+    // apagar esse conjunto de dados
   });
+
+  // roda todas as vezes, antes de CADA bloco
+  beforeEach(() => {
+    arr = [1, 2, 3];
+  });
+
+  // roda todas as vezes, depois de CADA bloco
+  afterEach(() => {
+
+  });
+
+  it('should have a size of 4 when push another value to the array', () => {
+    arr.push(4);
+    console.log(arr.length); // 4
+  });
+
+  it('should remove the value 3 when use pop in the array', () => {
+    console.log(arr.pop() === 3); // true
+  });
+
+  it('should have a size of 2 when pop a value from the array', () => {
+    arr.pop();
+    console.log(arr.length); // 2
+  });
+
 });
+/*
+describe('Main', () => {
+ describe('Method A', () => {
+   context('Context 1', () => {
+     it('should happen anything', () => {
+       throw new Error('just on error');
+     });
+   });
+
+   context.only('Context 2', () => {
+     it.skip('should happen blablabla', () => {
+       throw new Error('just o n error');
+     });
+
+     it('should happen mimimi', () => {
+     });
+   });
+
+ });
+
+ describe('Method B  ', () => {
+ });
+});
+*/
